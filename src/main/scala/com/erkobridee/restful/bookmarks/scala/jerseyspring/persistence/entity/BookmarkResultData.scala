93cc03543c1e0f7c16aaea5a40f08ce44e038c54
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-11-26 10:19:47 -0200

    backend - bookmark resultdata for pagination support

---
+++ src/main/scala/com/erkobridee/restful/bookmarks/scala/jerseyspring/persistence/entity/BookmarkResultData.scala
@@ -0,0 +1,31 @@
+package com.erkobridee.restful.bookmarks.scala.jerseyspring.persistence.entity
+
+import javax.xml.bind.annotation.XmlRootElement
+import scala.reflect.BeanProperty
+
+@XmlRootElement
+@scala.serializable
+class BookmarkResultData extends AbstractResultData[List[Bookmark]] {
+
+  @BeanProperty
+  var data : List[Bookmark] = null
+  
+  //----------------------------------------------------------------------------
+  // constructors
+  
+  def this(count: Int, page: Int, size: Int) {
+    this()
+    
+    super.updateInfo(count, page, size)
+  }
+  
+  def this(data: List[Bookmark], count: Int, page: Int, size: Int) {
+    this()
+    
+    this.data = data
+    super.updateInfo(count, page, size)
+  }
+  
+  //----------------------------------------------------------------------------
+  
+}
